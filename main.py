5ccd79ca9d740209830be311fb380a3b4faca1ec
mrnoone074 <96616764+mrnoone074@users.noreply.github.com>
2022-04-21 14:00:17 +0000

    i added keep_alive.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from keep_alive import keep_alive
 
 prefix = "!"
 bot = commands.Bot(command_prefix = prefix)
@@ -9,5 +10,6 @@ bot = commands.Bot(command_prefix = prefix)
 async def on_ready():
     print(f"{bot.user} is up!")
 
+keep_alive()
 # go and create a secret named exactly the same as the name under
 bot.run(os.getenv("token"))
