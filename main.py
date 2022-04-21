c26956c19e0c8616007e4d471b8e4a36c5aa5461
mrnoone074 <96616764+mrnoone074@users.noreply.github.com>
2022-04-21 17:45:29 +0000

    added some comments so its easier to understand.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# imports
 import discord
 from discord.ext import commands
 import os
@@ -6,10 +7,12 @@ from keep_alive import keep_alive
 prefix = "!"
 bot = commands.Bot(command_prefix = prefix)
 
+# here the console prints that your bot is logged in!
 @bot.event
 async def on_ready():
     print(f"{bot.user} is up!")
 
+# in the top of the output you see a link you can copy and paste in a monitor on www.uptimerobot.com
 keep_alive()
 # go and create a secret named exactly the same as the name under
 bot.run(os.getenv("token"))
